da2a74d6d2fc9a40b31646306fd92949f491eaa3
levent581 <levent581@BPC03>
2023-12-05 09:12:55 +0300

    Proje dosyası ekle.

---
+++ DegerliParemetreler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerliParemetreler
+{
+    class Sinifim
+    {
+        public void Selamla(string Ad = " Hasan")
+        {
+            Console.WriteLine("Merhaba "+Ad);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sinifim sinifim = new Sinifim();
+            sinifim.Selamla();
+            sinifim.Selamla("Efe ");
+        }
+    }
+}
